c6828ca91a74e507bd43246500beb9b7ae987c75
Bao Nguyen <bao.nguyen01@axonactive.com>
2019-07-15 13:48:37 +0700

    Seperate cell

--- lib/main.dart
+++ lib/main.dart
@@ -52,22 +52,7 @@ class _RealWoldState extends State<RealWorldApp> {
             itemCount: this.videos != null ? this.videos.length : 0,
             itemBuilder: (context, i) {
               final video = this.videos[i];
-              return new Column(
-                children: <Widget>[
-                  new Container(
-                    padding: new EdgeInsets.all(16.0),
-                    child: new Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        new Image.network(video['imageUrl']),
-                        new Container(height: 8.0),
-                        new Text(video['name'],
-                          style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                  )
-                ],
-              );
+              return new VideoCell(video);
             },
           ),
         ),
@@ -75,4 +60,32 @@ class _RealWoldState extends State<RealWorldApp> {
       debugShowCheckedModeBanner: false,
     );
   }
+}
+
+class VideoCell extends StatelessWidget {
+  final video;
+
+  VideoCell(this.video);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Container(
+          padding: new EdgeInsets.all(16.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Image.network(video['imageUrl']),
+              new Container(height: 8.0),
+              new Text(video['name'],
+                style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+        new Divider()
+      ],
+    );
+  }
+  
 }
