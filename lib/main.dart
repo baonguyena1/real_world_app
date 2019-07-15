6725f077c3f2a019f6e17dd7a926507027bad077
Bao Nguyen <bao.nguyen01@axonactive.com>
2019-07-15 13:42:06 +0700

    Show data from internet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(RealWorldApp());
@@ -12,12 +13,20 @@ class RealWorldApp extends StatefulWidget {
 
 class _RealWoldState extends State<RealWorldApp> {
   var _isLoading = true;
+  var videos;
 
-  _fetchData() {
+  _fetchData() async {
     print('Attempting fetch data from network');
     final url = 'https://api.letsbuildthatapp.com/youtube/home_feed';
-    final response = http.get(url);
-    print(response);
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final map = json.decode(response.body);
+      final videosJson = map['videos'];
+      setState(() {
+        this.videos = videosJson;
+        _isLoading = false;
+      });
+    }
   }
 
   @override
@@ -38,7 +47,29 @@ class _RealWoldState extends State<RealWorldApp> {
           ],
         ),
         body: new Center(
-          child: _isLoading ? new CircularProgressIndicator() : new Text('Finished loading...'),
+          child: _isLoading ? new CircularProgressIndicator() : 
+          new ListView.builder(
+            itemCount: this.videos != null ? this.videos.length : 0,
+            itemBuilder: (context, i) {
+              final video = this.videos[i];
+              return new Column(
+                children: <Widget>[
+                  new Container(
+                    padding: new EdgeInsets.all(16.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Image.network(video['imageUrl']),
+                        new Container(height: 8.0),
+                        new Text(video['name'],
+                          style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  )
+                ],
+              );
+            },
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
