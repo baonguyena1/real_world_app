7111ac8fed0125a2628ee50fb7e8e30b29c0d446
Bao Nguyen <bao.nguyen01@axonactive.com>
2019-07-15 16:24:59 +0700

    Using expanded

--- lib/views/course_info.dart
+++ lib/views/course_info.dart
@@ -7,21 +7,17 @@ class CourseInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
       children: <Widget>[
-        new Container(
-          child: new Row(
-            children: <Widget>[
-              new Image.network(this.course['imageUrl']),
-              new CourseGeneralInfo(this.course),
-            ],
-          ),
+      new Row(children: <Widget>[
+        Expanded(
+          child: new Image.network(this.course['imageUrl']),
+          flex: 1,
         ),
-        new Container(height: 8.0,),
-        new Divider()
-      ],
-    );
+        Expanded(child: new CourseGeneralInfo(this.course), flex: 2)
+      ],),
+      new Divider(),
+    ]);
   }
 }
 
@@ -32,18 +28,19 @@ class CourseGeneralInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Text(course['name'],
-            style: new TextStyle(fontSize: 16.0)),
-          new Text(course['duration'],
-            style: new TextStyle(fontStyle: FontStyle.italic),),
-          new Text('Episode #${course['number']}',
-            style: new TextStyle(fontWeight: FontWeight.bold),)
-        ],
-      ),
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        new Text(course['name'], style: new TextStyle(fontSize: 16.0)),
+        new Text(
+          course['duration'],
+          style: new TextStyle(fontStyle: FontStyle.italic),
+        ),
+        new Text(
+          'Episode #${course['number']}',
+          style: new TextStyle(fontWeight: FontWeight.bold),
+        )
+      ],
     );
   }
 }
